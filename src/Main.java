5faa2113cf6588d148b9122c473200ecfd6d9217
Hybusa <alkouzne007@gmail.com>
2023-01-13 19:50:17 +0300

    block completion(major struct changes)

--- src/Main.java
+++ src/Main.java
@@ -1,57 +1,82 @@
 public class Main
 {
-    public static void main(String[] args)
+    public static void main(String[] args) {
 
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+
+
+    }
+
+
+
+
+    public static void task1 ()
     {
-        //First tast
+        //First task
 //          У вас есть две переменных, в которых хранятся два целочисленных числа.
 //          Напишите программу, которая выводит в консоль результат деления первого числа на второе с остатком.
 //          В качестве исходных данных используйте значения для первого числа *– 47, для второго – 13):*
 //          В результате деления 47 */ 13*
 //          Выходит *3 и 6 в остатке*
 
-        int a1 = 47;
-        int b1 = 13;
+        int a = 47;
+        int b = 13;
 
-        int div_main = a1 / b1;
+        int div_main = a / b;
 
-        int div_add = a1 % b1;
+        int div_add = a % b;
 
         System.out.println("Задание 1:");
-        System.out.println(a1 + " / " + b1 + " = " + div_main + " и " + div_add + " в остатке.");
+        System.out.println(a + " / " + b + " = " + div_main + " и " + div_add + " в остатке.");
         System.out.print(System.lineSeparator());
 
+    }
+
+    public static void task2() {
         //Second task
 //        Напишите программу, которая вычисляет сумму цифр двухзначного числа.
 //        Программа должна работать с любым значением числа. В качестве исходных данных используйте число 44.
 
-        int a2  = 44;
+        int a  = 44;
 
-        int h_div2 = a2/10;
-        int w_div2 = a2 % 10;
+        int h_div = a/10;
+        int w_div = a % 10;
 
-        int sum2 = h_div2 + w_div2;
+        int sum = h_div + w_div;
 
         System.out.println("Задание 2:");
-        System.out.println(sum2);
+        System.out.println(sum);
         System.out.print(System.lineSeparator());
+
+    }
+
+    public static void task3() {
 
         //Third task
 
 //        Напишите программу, которая вычисляет сумму цифр трехзначного числа.
 //        Программа должна работать с любым значением числа. В качестве исходных данных используйте число 345.
 
-        int a3  = 345;
+        int a  = 345;
 
-        int d_div3 = a3/100;
-        int h_div3 = (a3/10)%10;
-        int w_div3 = a3 % 10;
+        int d_div = a/100;
+        int h_div = (a/10)%10;
+        int w_div = a % 10;
 
-        int sum3 = d_div3 + h_div3 + w_div3;
+        int sum = d_div + h_div + w_div;
 
         System.out.println("Задание 3:");
-        System.out.println(sum3);
+        System.out.println(sum);
         System.out.print(System.lineSeparator());
+
+    }
+
+    public static void task4() {
 
         //Fourth task
 /*
@@ -62,22 +87,25 @@ public class Main
         Отдельно выведите сумму скидки двух покупок.
 */
 
-        int a4_f = 4899;
-        int b4_c = 157;
-        int disc4 = 10;
+        int a_f = 4899;
+        int b_c = 157;
+        int disc = 10;
 
-        double disc_sum4 = (a4_f + b4_c)*disc4/100f;
-        double disc_sum4_r = Math.round(disc_sum4*100.0)/100.0;
+        double disc_sum = (a_f + b_c)*disc/100f;
+        double disc_sum_r = Math.round(disc_sum*100.0)/100.0;
 
-        double sum4 = (a4_f + b4_c) - disc_sum4_r;
-        double sum4_r = Math.round(sum4*100.0)/100.0;
+        double sum = (a_f + b_c) - disc_sum_r;
+        double sum_r = Math.round(sum*100.0)/100.0;
 
         System.out.println("Задание 4:");
-        System.out.println("Сумма покупки со скидкой: " + sum4_r + "р");
-        System.out.println("Скидка: " + disc_sum4_r + "р");
+        System.out.println("Сумма покупки со скидкой: " + sum_r + "р");
+        System.out.println("Скидка: " + disc_sum_r + "р");
         System.out.print(System.lineSeparator());
 
 
+    }
+
+    public static void task5() {
         //Fifth task
 
 /*
@@ -95,53 +123,58 @@ public class Main
 
         System.out.println("Задание 5:");
         // Часть А
-        int a5_a = 73;
-        int a5_a_disc = 15;
-        int a5_a_amount = 10;
+        int a_a = 73;
+        int a_a_disc = 15;
+        int a_a_amount = 10;
 
-        int a5_b = 95;
-        int a5_b_amount = 2;
+        int a_b = 95;
+        int a_b_amount = 2;
 
-        double a5_sum = (a5_a*a5_a_amount) - ((a5_a*a5_a_amount)*a5_a_disc/100f) + a5_b*a5_b_amount;
+        double a_sum = (a_a*a_a_amount) - ((a_a*a_a_amount)*a_a_disc/100f) + a_b*a_b_amount;
 
-        double a5_sum_r = Math.round(a5_sum*100.0)/100.0;
+        double a_sum_r = Math.round(a_sum*100.0)/100.0;
 
-        System.out.println("Сумма покупки A со скидкой: " + a5_sum_r + "р");
+        System.out.println("Сумма покупки A со скидкой: " + a_sum_r + "р");
         System.out.print(System.lineSeparator());
 
         // Часть Б
-        int b5_s = 2870;
-        int b5_s_disc = 50;
-        int b5_s_amount = 7;
+        int b_s = 2870;
+        int b_s_disc = 50;
+        int b_s_amount = 7;
 
-        int b5_b = 27;
-        int b5_b_amount = 1;
+        int b_b = 27;
+        int b_b_amount = 1;
 
-        double a5_s_sum = (b5_s*b5_s_amount) - (b5_s*b5_s_amount)*b5_s_disc/100f;
-        double a5_s_sum_r = Math.round(a5_s_sum*100.0)/100.0;
+        double a_s_sum = (b_s*b_s_amount) - (b_s*b_s_amount)*b_s_disc/100f;
+        double a_s_sum_r = Math.round(a_s_sum*100.0)/100.0;
 
-        double b5_b_sum = b5_b*b5_b_amount;
+        double b_b_sum = b_b*b_b_amount;
 
-        double b5_sum_r = a5_s_sum_r + b5_b_sum;
+        double b_sum_r = a_s_sum_r + b_b_sum;
 
-        System.out.println("Сумма покупки Б со скидкой: " + b5_sum_r + "р");
+        System.out.println("Сумма покупки Б со скидкой: " + b_sum_r + "р");
         System.out.print(System.lineSeparator());
 
         // Часть В
 
-        int c5_sm = 436;
-        double c5_sm_amount =  2.5;
+        int c_sm = 436;
+        double c_sm_amount =  2.5;
 
-        int c5_wm = 578;
-        double c5_wm_amount = 3.75;
+        int c_wm = 578;
+        double c_wm_amount = 3.75;
 
-        int c5_m_disc = 5;
+        int c_m_disc = 5;
 
-        double c5_m_sum = (c5_sm*c5_sm_amount) + (c5_wm*c5_wm_amount) - (c5_sm*c5_sm_amount)*c5_m_disc/100f;
-        double c5_m_sum_r = Math.round(c5_m_sum*100.0)/100.0;
+        double c_m_sum = (c_sm*c_sm_amount) + (c_wm*c_wm_amount) - (c_sm*c_sm_amount)*c_m_disc/100f;
+        double c_m_sum_r = Math.round(c_m_sum*100.0)/100.0;
 
-        System.out.println("Сумма покупки В со скидкой: " + c5_m_sum_r + "р");
+        System.out.println("Сумма покупки В со скидкой: " + c_m_sum_r + "р");
         System.out.print(System.lineSeparator());
+
+
+    }
+
+    public static void task6() {
 
         //Sixth task
 
@@ -166,6 +199,5 @@ public class Main
         System.out.println("Конец первого блока '1.2 Переменные' ");
 
     }
-
-
+    
 }
